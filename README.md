47a7dbd2e7cbb842560a5bb9c46c3f15d0d260d5
Oliver Merkel <Merkel.Oliver@web.de>
2015-08-07 22:46:39 +0200

    Add index.html plus related components.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 =============
 
 Caesar Cipher / Shift Cipher encryption technique based tool.
+
+The basic concept of the Caesar Cipher is one of the encryption techniques
+that should have been used by Gaius Iulius Caesar (*July 100BC; &dagger;March
+15th, 44BC) to protect message exchange among his military troops.
+
+Caesar Cipher encryption is basically performed by monoalphabetic character
+substitution applying a positional shift in a given alphabet. Reversing
+the shift by same amount of positions with same alphabet decrypts the
+ciphered text into plain text again. Such that the secret key in this
+symmetric cipher is represented by the number respresenting the amount
+of positions to shift and the usage of the correct alphabet to be applied.
+
--- html5/src/js/hmi.js
+++ html5/src/js/hmi.js
@@ -19,25 +19,38 @@
  *
  */
 function Hmi() {
+  // this.FILEBACKGROUND = 'img/board.jpg';
+  this.ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
   this.caesar = new CipherCaesar();
 }
-
-Hmi.FILEBACKGROUND = 'img/board.jpg';
 
 Hmi.prototype.init = function () {
   var $window = $(window);
   $window.resize( this.resize.bind( this ) );
+  this.reset();
   this.resize();
-  this.decrypt();
   $('#decreasedecryptshift').on('click', this.decreaseDecryptShift.bind(this));
   $('#increasedecryptshift').on('click', this.increaseDecryptShift.bind(this));
   $('#decryptalphabetstandard').on('change', this.decrypt.bind(this));
   $('#decryptalphabetcustom').on('change', this.decrypt.bind(this));
-  this.encrypt();
   $('#decreaseencryptshift').on('click', this.decreaseEncryptShift.bind(this));
   $('#increaseencryptshift').on('click', this.increaseEncryptShift.bind(this));
   $('#encryptalphabetstandard').on('change', this.encrypt.bind(this));
   $('#encryptalphabetcustom').on('change', this.encrypt.bind(this));
+  $('#reset').on('click', this.reset.bind(this));
+};
+
+Hmi.prototype.reset = function () {
+  $('#caesarcipheredtext').val('Oittqi mab wuvqa lqdqai qv xizbma bzma, ' +
+    'ycizcu cviu qvkwtcvb Jmtoim, itqiu Iycqbivq, bmzbqiu, ycq qxawzcu ' +
+    'tqvoci Kmtbim, vwabzi Oittq ixxmttivbcz.').css('height', 0).keyup();
+  $('#plaintext').val('This is the secret message to be encrypted.').css('height', 0).keyup();
+  $('#decryptshift').html('0');
+  $('#decryptcustomalphabet').val(this.ALPHABET); 
+  $('#encryptshift').html('0');
+  $('#encryptcustomalphabet').val(this.ALPHABET);  
+  this.decrypt();
+  this.encrypt();
 };
 
 Hmi.prototype.decreaseDecryptShift = function () {
@@ -57,8 +70,8 @@ Hmi.prototype.decrypt = function () {
   var isStandardAlphabet = 'choice-decryptalphabetstandard' == choiceAlphabetValue;
   var customAlphabet = $('#decryptcustomalphabet').val();
   var alphabets = isStandardAlphabet ? [
-    'abcdefghijklmnopqrstuvwxyz',
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    this.ALPHABET.toLowerCase(),
+    this.ALPHABET.toUpperCase()
   ] : [
     customAlphabet.toLowerCase(),
     customAlphabet.toUpperCase()
@@ -86,8 +99,8 @@ Hmi.prototype.encrypt = function () {
   var isStandardAlphabet = 'choice-encryptalphabetstandard' == choiceAlphabetValue;
   var customAlphabet = $('#encryptcustomalphabet').val();
   var alphabets = isStandardAlphabet ? [
-    'abcdefghijklmnopqrstuvwxyz',
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    this.ALPHABET.toLowerCase(),
+    this.ALPHABET.toUpperCase()
   ] : [
     customAlphabet.toLowerCase(),
     customAlphabet.toUpperCase()
@@ -107,6 +120,17 @@ Hmi.prototype.resize = function () {
   $('#game-page').css({
     'background-size': 'auto ' + (size/6) + 'px',
   });
+  var minSize = 32;
+  size = 0.06 * availableWidth < minSize ? minSize : 0.06 * availableWidth;
+  $('#customMenu').css({
+    'width': size+'px', 'height': size+'px',
+    'background-size': size+'px ' + size+'px',
+  });
+  $('#customBackAbout').css({
+    'width': size+'px', 'height': size+'px',
+    'background-size': size+'px ' + size+'px',
+  });
+  $('#main').css({ 'padding-top': size+'px', });
 };
 
 $(function() { (new Hmi()).init(); });
